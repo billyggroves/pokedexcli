c8e3b1e890a3b307e4d60005359f0fcb3bf76094
billyggroves <billyggroves@gmail.com>
2026-01-11 11:38:41 -0500

    syncing

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"time"
 
-	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
+	"github.com/billyggroves/pokedexcli/internal/pokeapi"
 )
 
 func main() {
--- repl.go
+++ repl.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"strings"
 
-	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
+	"github.com/billyggroves/pokedexcli/internal/pokeapi"
 )
 
 type config struct {
